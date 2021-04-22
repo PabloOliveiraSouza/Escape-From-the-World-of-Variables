e513f52daa54f404185985480fba590285fd5e25
hyper-developer-555 <pablodouza34@gmail.com>
2021-04-21 21:13:34 -0300

    Initial commit

---
+++ src/efwv/Container.java
@@ -0,0 +1,24 @@
+package efwv;
+
+import javax.swing.JFrame;
+
+import efwv.Modelo.Fase;
+
+public class Container extends JFrame {
+	
+	public Container() {
+
+		add(new Fase());
+		setTitle("EFWV (Escape from the World of Variables)");
+		setSize(1024, 768);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
+		setVisible(true);
+
+	}
+	public static void main(String[] args) {
+		new Container();
+
+	}
+}
